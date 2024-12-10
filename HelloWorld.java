660820397180a013366c022204ccc82b2417097e
SangeethaKumari <sangeethakumari.g@gmail.com>
2024-12-10 11:03:19 -0800

    modified:   HelloWorld.java :wq

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,10 +1,10 @@
 class HelloWorld{
     public static void main(String args[]){
         System.out.print.ln("Learning git as well as selenium testing")
-        
-    }
 
+    }
+    // this method adds the two input values
     public int static getSum(int a, int b){
-        return a+b;
+        return a+b; 
     }
 }
