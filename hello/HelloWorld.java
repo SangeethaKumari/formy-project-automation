7c791c7badd7b83f48fce7ae71e7518cc7546347
SangeethaKumari <sangeethakumari.g@gmail.com>
2024-12-10 11:44:56 -0800

    moved helloworld.java file to new folder hello

---
+++ hello/HelloWorld.java
@@ -0,0 +1,15 @@
+public class HelloWorld{
+    public static void main(String args[]){
+        System.out.print.ln("Learning git as well as selenium testing")
+
+    }
+    // this method adds the two input values
+    public int static getSum(int a, int b){
+        return a+b; 
+    }
+
+     // this method get the difference the two input values
+    public int static getDiff(int a, int b){
+        return a-b; 
+    }
+}
