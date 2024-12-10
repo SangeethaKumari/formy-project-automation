53519a76a986bdb3df2215288ad16722823cafc3
SangeethaKumari <sangeethakumari.g@gmail.com>
2024-12-10 10:41:43 -0800

    learning git with selenium

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+class HelloWorld{
+    public static void main(String args[]){
+        System.out.print.ln("Learning git as well as selenium testing")
+        
+    }
+
+    public int static getSum(int a, int b){
+        return a+b;
+    }
+}
